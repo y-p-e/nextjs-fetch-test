d12ba8ce6a0ba990a8e938e0f4290b29f8f84ea8
Yuki Tanio <yuki_tanio@yahoo.co.jp>
2022-09-04 15:06:56 +0900

    リライト機能によるCORS回避

--- pages/users/csr.tsx
+++ pages/users/csr.tsx
@@ -1,3 +1,4 @@
+/* eslint no-unused-vars: 0 */
 import type {
   NextPage,
 } from 'next'
@@ -6,12 +7,19 @@ import {User} from '../../services/users/get-users'
 import useSWR from 'swr'
 import { fetcher } from "../../utils"
 
+const UserList = (props:any) => {
+  return (
+		<>
+			<p>{`username: ${props.user.username}`}</p>
+			<p>{`email: ${props.user.email}`}</p>
+		</>
+  );
+}
 
 const CSR: NextPage = () => {
-	const {data, error, mutate} = useSWR<User[]>('http://localhost:8080/users', fetcher, {shouldRetryOnError: false})
-	console.log(data)
+	const {data, error, mutate} = useSWR<User[]>('/api/users/csr', fetcher, {shouldRetryOnError: false})
 	const onClickUser = () => {
-		mutate(data, false)
+		mutate(data)
 	}
 
 	if (data === undefined) {
@@ -27,13 +35,12 @@ const CSR: NextPage = () => {
 		<div>
 			<h1>CSRによるユーザー一覧情報取得</h1>
 			<button onClick={onClickUser}>ユーザー情報取得</button>
-			{	data.map((user: User) => (
-					<>
-						<p>{`username: ${user.username}`}</p>
-						<p>{`email: ${user.email}`}</p>
-					</>
+			<div>
+			{	data.map((user: User, index: number) => (
+				<UserList user={user} key={index}/>
 				))
 			}
+			</div>
 		</div>
 	)
 }
